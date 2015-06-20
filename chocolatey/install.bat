0466d6499952e1f28b6829d477bada22e9be847e
Ondrej Pokorny <pokorny.ondrej@gmail.com>
2015-06-20 12:04:41 +0200

    chocolatey install script

---
+++ chocolatey/install.bat
@@ -0,0 +1,14 @@
+choco install virtuawin -y
+choco install conemu -y
+choco install sublimetext3 -y
+choco install hxd -y
+choco install firefox -y
+choco install vlc -y
+choco install gimp -y
+choco install git -y
+choco install gitextensions -y
+choco install inkscape -pre -y
+choco install cdburnerxp -y
+choco install devbox-rapidee -y
+choco install procexp -y
+choco install atom -y
