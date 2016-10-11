fc66a0f26a4e3458b8ae83b44c35a09539962150
Ondrej Pokorny <Ondrej Pokorny>
2016-10-11 22:49:48 +0200

    update

--- chocolatey/install.bat
+++ chocolatey/install.bat
@@ -1,5 +1,5 @@
 :: choco install virtuawin -y
-choco install conemu -y
+:: choco install conemu -y
 choco install sublimetext3 -y
 choco install hxd -y
 choco install firefox -y
@@ -9,7 +9,7 @@ choco install git -y
 choco install gitextensions -y
 choco install inkscape -pre -y
 :: choco install cdburnerxp -y
-:: choco install devbox-rapidee -y
+choco install devbox-rapidee -y
 choco install procexp -y
 choco install atom -y
 choco install sourcetree -y
@@ -17,3 +17,6 @@ choco install wireshark -y
 choco install emacs -y
 choco install rufus -y
 choco install visualstudiocode -y
+choco install dropbox -y
+choco install cmder -y
+choco install libreoffice -y
