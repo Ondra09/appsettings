ab2bf1282c56ddc8e2cdd55d0c367343b6e20fe8
Ondrej Pokorny <pokorny.ondrej@gmail.com>
2017-02-04 12:38:45 +0100

    choco update

--- chocolatey/install.bat
+++ chocolatey/install.bat
@@ -1,24 +1,25 @@
-:: choco install virtuawin -y
-:: choco install conemu -y
-choco install sublimetext3 -y
-choco install hxd -y
-choco install firefox -y
-choco install filezilla -y
-choco install vlc -y
-choco install gimp -y
-choco install git -y
-choco install gitextensions -y
-choco install inkscape -pre -y
-:: choco install cdburnerxp -y
-choco install devbox-rapidee -y
-choco install procexp -y
-choco install atom -y
-choco install sourcetree -y
-choco install wireshark -y
-choco install emacs -y
-choco install rufus -y
-choco install visualstudiocode -y
-choco install dropbox -y
-choco install cmder -y
-choco install libreoffice -y
-choco install haroopad -y
+:: choco install virtuawin -y --force
+:: choco install conemu -y --force
+:: choco install sublimetext3 -y --force
+choco install hxd -y --force
+:: choco install firefox -y --force
+:: choco install filezilla -y --force
+choco install vlc -y --force
+::choco install gimp -y --force
+:: choco install git -y --force
+choco install gitextensions -y --force
+choco install inkscape -pre -y --force
+:: choco install cdburnerxp -y --force
+choco install rapidee  -y --force
+choco install procexp -y --force
+:: choco install atom -y --force
+:: choco install sourcetree -y --force
+:: choco install wireshark -y --force
+:: choco install emacs -y --force
+:: choco install rufus -y --force
+choco install visualstudiocode -y --force
+choco install dropbox -y --force
+choco install cmder -y --force
+choco install libreoffice -y --force
+choco install haroopad -y --force
+choco install gitk -y --force
